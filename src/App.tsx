cb2dc4fd00cf64105caac7069dfd4e2aacf6e9bc
Damitha <hellodamitha@gmail.com>
2024-06-12 19:59:17 +0530

    reactjs installation with tailwind + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
